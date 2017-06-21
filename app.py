06bb34ef339eafcdfbe1729801ed10be434bd16f
Jason Fiacco <jason.fiacco@yale.edu>
2017-06-21 09:58:01 -0400

    Replaced ID attribute with public URI for each task

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ tasks = [
 
 @app.route('/todo/api/v1.0/tasks', methods = ['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -46,7 +46,7 @@ def create_task():
     tasks.append(task)
     return jsonify( {'task': task} ), 201
 
-@app.route('/todod/api/v1.0/tasks/<int: task_id>', methods = ['PUT'])
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
@@ -64,13 +64,22 @@ def update_task(task_id):
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
-@app.route('todo/api/v1.0/tasks/<int :task_id>', methods = ['DELETE'])
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
 def delete_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
     return jsonify({'result': True})
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
 
 if __name__ == '__main__':
     app.run(debug = True)
